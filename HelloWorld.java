e27459dd2b9d591221a09bb472449a97d76b32cd
Hirondina Correia <hirondina.nosi@gmail.com>
2017-11-10 15:06:24 -0100

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+package projetojava;
+public class HelloWorld {
+    public static void main(String[] args) {
+       for(int i=1;i<=10;i++){
+            System.out.println("HelloWorld..."+i);
+    }
+    }
+    
+}
